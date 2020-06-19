2885bb87173cfaba80f1b8bdc50be92692f689b0
TenDan <tytusworld@gmail.com>
2020-06-19 17:17:38 +0200

    Update the code

--- src/main/java/HelloFX.java
+++ src/main/java/HelloFX.java
@@ -1,23 +1,21 @@
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+//import javafx.scene.control.Label;
+//import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
 public class HelloFX extends Application {
-
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Application");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.resizableProperty().setValue(false);
+        primaryStage.show();
     }
-
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
